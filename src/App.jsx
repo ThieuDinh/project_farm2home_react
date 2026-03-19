17df9fcafcc72e5aeeaaa5f56d59a9d8460578dc
HLuan291 <lehuuluan2912004@gmail.com>
2026-03-20 00:19:15 +0700

    Them giao dien trang chu

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
-import Hero from './components/Hero';
-import ProductCategories from './components/ProductCategories';
-import StorySection from './components/StorySection';
 import Footer from './components/Footer';
-
+import Home from './pages/home';
 const App = () => {
   return (
     <div className="text-[#5c5750] font-sans bg-[#fafafa]">
-      <Header />
       <main>
-        <Hero />
-        <ProductCategories />
-        <StorySection />
+        <Home />
       </main>
-      <Footer />
     </div>
   );
 };
