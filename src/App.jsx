0ef28d579ee69b8c6c5c37c01a0cbbd0de270f78
ThieuDinh <dinhthieubrvt@gmail.com>
2026-04-04 15:16:13 +0700

    deploy: add file to deploy 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 
 import Header from './components/Header';
-import Footer from './components/Footer';
-//import Home from './pages/Home'; 
+import Footer from './components/Footer';import Home from './pages/Home'; 
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ProfilePage from './pages/ProfilePage';
